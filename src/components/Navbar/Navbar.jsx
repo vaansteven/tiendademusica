b65598249f30c2e581ad8e060df4b241af15e637
Esteban <estebanmarquez.c@gmail.com>
2022-11-03 12:59:54 -0300

    cambios 4

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
+import CartWidget from './CartWidget';
+import { NavLink, Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className="navbar">
+        <h1 className="title">
+        <Link className="links" to="/">
+        Music Mind
+    </Link>
+       </h1>
             <ul>
-                <li>Nosotros</li>
-                <li>Ofertas</li>
-                <li>Productos</li>
-                <li>Contacto</li>
+                <li> 
+                <NavLink className="links" to="/category/consolas">
+                    Consolas
+                </NavLink>
+                </li>
+                <li>
+                <NavLink className="links" to="/category/auriculares">
+                Auriculares
+                </NavLink>
+                </li>
+                <li> 
+                <NavLink className="links" to="/category/merchandise">
+                Merchandise
+                </NavLink>
+                </li>
+                <li>  <CartWidget /></li>
             </ul>
+          
         </nav>
     );
 };
