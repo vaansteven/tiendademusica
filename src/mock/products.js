26057e38de881f0acf9555b70c2dbc4e648e2a9c
Esteban <estebanmarquez.c@gmail.com>
2022-11-26 05:36:31 -0300

    products

--- src/mock/products.js
+++ src/mock/products.js
@@ -1,17 +1,16 @@
 export const products = [
-   
     {
-        id: 2,
-        descuento: 10,
-        title: 'Denon MCX 8000',
-        price: 3500,
-        stock: 15,
+        
+        descuento: 15,
+        title: 'Numark Mixtrack Pro FX',
+        price: 2900,
+        stock: 10,
         category: 'consolas',
-        description:'CONTROLADOR Y REPRODUCTOR AUTÓNOMO PARA DJ DE 4 CANALES. Incluye la revolucionaria tecnología autónoma Denon DJ Engine.2 tomas USB para reproducción Engine en modo autónomo.Incluye software Serato DJ Pro. 2 displays de alta definición que muestran el uso con Engine y Serato. Mezclador digital profesional de 4 canales con 2 entradas de micrófono. 3 efectos integrados para reproducción Engine y entradas de línea. Pads sensibles a la velocidad para cues, Rolls, slices y samples. Conexión de red StagelinQ para control de iluminación y vídeo.Serato DVS Upgrade Ready. Construcción metálica.',
-        img: 'https://res.cloudinary.com/dobpipof2/image/upload/v1667432979/Music%20mind/Denon-DJ-MCX8000-2_mhtpyj.jpg',
+        description: 'El Mixtrack Platinum FX tiene todos los elementos esenciales que necesitas para sonar como un DJ profesional! En esencia, Mixtrack Platinum FX es un controlador de DJ digital de 4 decks y 24 bits con una sección de efectos de software dedicada, jog wheels táctiles capacitivas grandes de 6 pulgadas con pantallas de alta resolución integradas y 16 pads de rendimiento multifuncionales. Listo para asumir cualquier estilo de DJ y perfecto para pequeños eventos, fiestas y para desarrollar sus habilidades de DJ, Mixtrack Platinum FX fusiona la funcionalidad solicitada con todas las herramientas para crear su estilo característico: comienza su punto de entrada a un mundo de actuaciones de DJ sin restricciones. ¡aquí!',
+        img: 'https://res.cloudinary.com/dobpipof2/image/upload/v1669401922/Music%20mind/Numark_MixtrackProFX_angle_web_yulxga.jpg',
     },
     {
-        id: 3,
+        
         descuento: 10,
         title: 'Consola Pioneer ddj sx2',
         price: 2000,
@@ -20,8 +19,18 @@ export const products = [
         description:'El controlador DDJ-SX2 de 4 canales se basa en el éxito de su predecesor, pero hereda dos características imprescindibles del DDJ-SZ de primer nivel: Performance Pads con LED de puntos de entrada de varios colores y una cuenta regresiva de entrada en jog. Además, está listo para la actualización de Serato DJ DVS y cuenta con una latencia mejorada de jog wheel, lo que lo convierte en el mejor compañero de scratch DJ.El DDJ-SX2 te ofrece una manipulación nativa de Serato Flip; un paquete de expansión Serato DJ Pro que le brinda el poder de grabar y reproducir secuencias hot cue sobre la marcha.',
         img: 'https://res.cloudinary.com/dobpipof2/image/upload/v1667432979/Music%20mind/controlador-dj-pioneer-ddj-sx2-21-dead82bc77e541330015209681205225-1024-1024_pbf2bf.jpg',
     },
+    {
+        
+        descuento: 10,
+        title: 'Denon MCX 8000',
+        price: 3500,
+        stock: 15,
+        category: 'consolas',
+        description:'Controlador y Reproductor autónomo de 4 canales. Incluye la revolucionaria tecnología autónoma Denon DJ Engine.2 tomas USB para reproducción Engine en modo autónomo.Incluye software Serato DJ Pro. 2 displays de alta definición que muestran el uso con Engine y Serato. Mezclador digital profesional de 4 canales con 2 entradas de micrófono. 3 efectos integrados para reproducción Engine y entradas de línea. Pads sensibles a la velocidad para cues, Rolls, slices y samples. Conexión de red StagelinQ para control de iluminación y vídeo.Serato DVS Upgrade Ready. Construcción metálica.',
+        img: 'https://res.cloudinary.com/dobpipof2/image/upload/v1667432979/Music%20mind/Denon-DJ-MCX8000-2_mhtpyj.jpg',
+    },
    {
-        id: 5,
+        
         descuento: 10,
         title: 'Sennheiser HD 200 Pro',
         price: 180,
@@ -31,17 +40,7 @@ export const products = [
         img: 'https://res.cloudinary.com/dobpipof2/image/upload/v1667434653/Music%20mind/product_detail_x2_desktop_Sennheiser-Product-HD200PRO-Angled_tlg86h.jpg',
     },
     {
-        id: 4,
-        descuento: 10,
-        title: 'Sennheiser HD 25',
-        price: 250,
-        stock: 15,
-        category: 'auriculares',
-        description:'Considerado como el "estándar de la industria" para los DJ, el HD 25 se puede encontrar en las cabinas de DJ de todo el mundo. Funciona excepcionalmente bien en entornos ruidosos. Debido a su bajo peso y la opción de escuchar con un solo oído, los auriculares HD 25 son indispensables para el monitoreo móvil. Los HD 25 cerrados son audífonos de monitoreo profesional diseñados específicamente que ofrecen una alta atenuación del ruido de fondo. Capaces de manejar niveles de presión de sonido muy altos y de construcción extremadamente robusta, estos auriculares funcionan excepcionalmente bien en entornos ruidosos, p. ENG, sonorización, monitorización en estudio y pruebas de equipos de audio. Audífonos de monitoreo ideales para camarógrafos y DJ, estos son un par de audífonos de trabajo para verdaderos profesionales del sonido.',
-        img: 'https://res.cloudinary.com/dobpipof2/image/upload/v1667434652/Music%20mind/product_detail_x1_desktop_sennheiser-new-hd-25-light-isofront_orlslh.jpg',
-    },
-    {
-        id: 6,
+        
         descuento: 10,
         title: 'Pioneer HDJ X10',
         price: 300,
@@ -51,27 +50,17 @@ export const products = [
         img: 'https://res.cloudinary.com/dobpipof2/image/upload/v1667438036/Music%20mind/K45286000001000-00-720x720_zildem.jpg',
     },
     {
-        id: 7,
-        descuento: 5,
-        title: 'Pulsera Ultra',
-        price: 15,
-        stock: 100,
-        category: 'merchandise',
-        description:'Pulsera de silicona, impresa en laser logo y slogan del ULTRA Music Festival. Disponible en 10 colores. Cuál será el tuyo?',
-        img: 'https://res.cloudinary.com/dobpipof2/image/upload/v1667438141/Music%20mind/misc_20merchandise_202016-1451-Edit_800x.jpg_x1j0g6.webp',
+        
+        descuento: 10,
+        title: 'Sennheiser HD 25',
+        price: 250,
+        stock: 15,
+        category: 'auriculares',
+        description:'Considerado como el "estándar de la industria" para los DJ, el HD 25 se puede encontrar en las cabinas de DJ de todo el mundo. Funciona excepcionalmente bien en entornos ruidosos. Debido a su bajo peso y la opción de escuchar con un solo oído, los auriculares HD 25 son indispensables para el monitoreo móvil. Los HD 25 cerrados son audífonos de monitoreo profesional diseñados específicamente que ofrecen una alta atenuación del ruido de fondo. Capaces de manejar niveles de presión de sonido muy altos y de construcción extremadamente robusta, estos auriculares funcionan excepcionalmente bien en entornos ruidosos, p. ENG, sonorización, monitorización en estudio y pruebas de equipos de audio. Audífonos de monitoreo ideales para camarógrafos y DJ, estos son un par de audífonos de trabajo para verdaderos profesionales del sonido.',
+        img: 'https://res.cloudinary.com/dobpipof2/image/upload/v1667434652/Music%20mind/product_detail_x1_desktop_sennheiser-new-hd-25-light-isofront_orlslh.jpg',
     },
-    // {
-    //     id: 8,
-    //     descuento: 10,
-    //     title: 'Buzo Tomorrowland Black',
-    //     price: 1000,
-    //     stock: 30,
-    //     category: 'merchandise',
-    //     description: ',
-    //     img: 'https://res.cloudinary.com/dobpipof2/image/upload/v1667438141/Music%20mind/Tomorrowland-merchandise-graphic-icon-sweater-the-music-essentials_jlgd4s.jpg',
-    // },
     {
-        id: 9,
+        
         descuento: 10,
         title: 'Snapback Ultra ',
         price: 50,
@@ -81,17 +70,17 @@ export const products = [
         img: 'https://res.cloudinary.com/dobpipof2/image/upload/v1667438141/Music%20mind/ultra-music-snapback-hat-black_gtrjet.jpg',
     },
     {
-        id: 1,
-        descuento: 15,
-        title: 'Numark Mixtrack Pro FX',
-        price: 2900,
-        stock: 10,
-        category: 'consolas',
-        description: 'El Mixtrack Platinum FX tiene todos los elementos esenciales que necesitas para sonar como un DJ profesional! En esencia, Mixtrack Platinum FX es un controlador de DJ digital de 4 decks y 24 bits con una sección de efectos de software dedicada, jog wheels táctiles capacitivas grandes de 6 pulgadas con pantallas de alta resolución integradas y 16 pads de rendimiento multifuncionales. Listo para asumir cualquier estilo de DJ y perfecto para pequeños eventos, fiestas y para desarrollar sus habilidades de DJ, Mixtrack Platinum FX fusiona la funcionalidad solicitada con todas las herramientas para crear su estilo característico: comienza su punto de entrada a un mundo de actuaciones de DJ sin restricciones. ¡aquí!',
-        img: 'https://res.cloudinary.com/dobpipof2/image/upload/v1667432979/Music%20mind/numark_controllers_mtpfx_a9ywqu.png',
+        
+        descuento: 5,
+        title: 'Pulsera Ultra',
+        price: 15,
+        stock: 100,
+        category: 'merchandise',
+        description:'Pulsera de silicona, impresa en laser logo y slogan del ULTRA Music Festival. Disponible en 10 colores. Cuál será el tuyo?',
+        img: 'https://res.cloudinary.com/dobpipof2/image/upload/v1667438141/Music%20mind/misc_20merchandise_202016-1451-Edit_800x.jpg_x1j0g6.webp',
     },
     {
-        id: 10,
+        
         descuento: 10,
         title: 'Remera Ultra ',
         price: 50,
@@ -111,7 +100,7 @@ export const getProducts = (categoryName) => {
         const ref = categoryName ? prodFiltrados : products;
         setTimeout(() => {
             res(ref);
-        }, 100);
+        }, 150);
     });
 };
 
